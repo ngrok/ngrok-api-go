f007818c632cab70510e6488c3884ba5b6ab0961
Jonathan Stacks <jonstacks@users.noreply.github.com>
2024-12-17 15:08:11 -0600

    chore: Generate client

--- bot_users/client.go
+++ bot_users/client.go
@@ -95,7 +95,7 @@ func (c *Client) Get(ctx context.Context, id string) (*ngrok.BotUser, error) {
 // List all bot users in this account.
 //
 // https://ngrok.com/docs/api#api-bot-users-list
-func (c *Client) List(paging *ngrok.Paging) *Iter {
+func (c *Client) List(paging *ngrok.Paging) ngrok.Iter[*ngrok.BotUser] {
 	if paging == nil {
 		paging = new(ngrok.Paging)
 	}
@@ -112,16 +112,16 @@ func (c *Client) List(paging *ngrok.Paging) *Iter {
 		queryVals.Set("limit", *paging.Limit)
 	}
 	apiURL.RawQuery = queryVals.Encode()
-	return &Iter{
+	return &iterBotUser{
 		client:   c,
 		n:        -1,
 		nextPage: apiURL,
 	}
 }
 
-// Iter allows the caller to iterate through a list of values while
+// iter allows the caller to iterate through a list of values while
 // automatically fetching new pages worth of values from the API.
-type Iter struct {
+type iterBotUser struct {
 	client *Client
 	n      int
 	items  []ngrok.BotUser
@@ -132,7 +132,7 @@ type Iter struct {
 
 // Next returns true if there is another value available in the iterator. If it
 // returs true it also advances the iterator to that next available item.
-func (it *Iter) Next(ctx context.Context) bool {
+func (it *iterBotUser) Next(ctx context.Context) bool {
 	// no more if there is an error
 	if it.err != nil {
 		return false
@@ -182,14 +182,14 @@ func (it *Iter) Next(ctx context.Context) bool {
 
 // Item() returns the BotUser currently
 // pointed to by the iterator.
-func (it *Iter) Item() *ngrok.BotUser {
+func (it *iterBotUser) Item() *ngrok.BotUser {
 	return &it.items[it.n]
 }
 
 // If Next() returned false because an error was encountered while fetching the
 // next value Err() will return that error. A caller should always check Err()
 // after Next() returns false.
-func (it *Iter) Err() error {
+func (it *iterBotUser) Err() error {
 	return it.err
 }
 
